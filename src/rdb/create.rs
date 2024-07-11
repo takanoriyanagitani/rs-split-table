051ffca18010fb93ac26b02f9ae8f20823c15a57
yanagitani <takanori.yanagitani@gmail.com>
2024-07-12 08:12:11 +0900

    docs: more docs

--- src/rdb/create.rs
+++ src/rdb/create.rs
@@ -1,5 +1,8 @@
+//! Traits to create a table.
+
 use tonic::Status;
 
+/// Creates a table with the specified name.
 #[tonic::async_trait]
 pub trait CreateTable: Sync + Send + 'static {
     async fn create(&self, name: &str) -> Result<(), Status>;
